96ecdd85f6e3bc9f8e23cc64dc9cc46761007b37
JimmyLam9758 <JimmyLam9758@gmail.com>
2024-04-20 11:54:22 +1000

    feat: css started

--- src/scripts/dataManager.js
+++ src/scripts/dataManager.js
@@ -1,70 +1,94 @@
-// Declare JS data that we want to store
+// Declare JS data that we want to store 
 let cssThemes = [
 
 ];
 
 let pageTheme = "dark";
 
+
+
 /**
  * Retrieve, assign, and return the latest stored CSS theme list from the browser localstorage.
- * 
- * @returns Array of objects
+ * @author BigfootDS
+ *
+ * @returns Array of objects. 
  */
 function getStoredCssThemes(){
-    // Update the CssTheme array
-    let rawJsonStringCssThemes = localStorage.getItem("cssThemes");
-    cssThemes = JSON.parse(rawJsonStringCssThemes);
-    // return the updated data
-    return cssThemes;
+	// Update the cssTheme array
+	let rawJsonStringCssThemes = localStorage.getItem("cssThemes");
+	cssThemes = JSON.parse(rawJsonStringCssThemes);
+	// Return the updated data 
+	return cssThemes;
 }
+
 
 /**
  * Retrieve, assign, and return the latest stored page theme from the browser localstorage.
- * 
+ * @author BigfootDS
+ *
  * @returns String. Should be "light" or "dark".
  */
 function getStoredPageTheme(){
-    pageTheme = localStorage.getItem("pageTheme")
-    return pageTheme;
+	pageTheme = localStorage.getItem("pageTheme");
+	return pageTheme;
 }
 
 
-// Create/Update localstorage 
+// Create/Update localstorage
 function setCssThemesToStorage(){
-    let dataAsJsonString = JSON.stringify(cssThemes);
-    localStorage.setItem("cssThemes", dataAsJsonString);
+	let dataAsJsonString = JSON.stringify(cssThemes);
+	localStorage.setItem("cssThemes", dataAsJsonString);
 }
 
 function setPageThemeToStorage(){
 	localStorage.setItem("pageTheme", pageTheme);
 }
 
+
 // Check if data exists,
 // if it does, retrieve it,
 // else, set default variables to localstorage
-if (localStorage.getItem("pageTheme") && localStorage.getItem("cssThemes").length > 0){
-    getStoredCssThemes();
-    getStoredPageTheme();
+if (localStorage.getItem("pageTheme") && localStorage.getItem("cssThemes")?.length > 0){
+	getStoredCssThemes();
+	getStoredPageTheme();
 } else {
-    setCssThemesToStorage();
-    setPageThemeToStorage();
+	setCssThemesToStorage();
+	setPageThemeToStorage();
 }
 
 
+
+
+
+
+
+
+
+
 /*
+
 cssThemes = [
-    {
-        name: "blah blah 100",
-        hex: "000000"
-    },
-    {
-        name: "blah blah 100",
-        hex: "000000"
-    }
+	{
+		name: "blah blah 100",
+		hex: "000000"
+	},
+	{
+		name: "blah blah 100",
+		hex: "000000"
+	}
 ]
+
 "[[Object object], [Object object]]"
+
 JSON.stringify(cssThemes);
+
 "[{'name':'blah blah 100', 'hex': '000000'}, {'name':'blah blah 100', 'hex': '000000'}]""
+
 */
 
-// Delete localstorage
+
+
+
+
+
+// Delete localstorage 
--- src/scripts/pageThemeManager.js
+++ src/scripts/pageThemeManager.js
@@ -1,30 +1,30 @@
 const themes = {
-    light: {
-        "50":"#f7fbff",
-        "100":"#f0f6ff",
-        "200":"#e0eeff",
-        "300":"#d1e5ff",
-        "400":"#c2dcff",
-        "500":"#b3d4ff",
-        "600":"#6babff",
-        "700":"#2483ff",
-        "800":"#005fdb",
-        "900":"#004094",
-        "950":"#003170"
-    },
-    dark: {
-        "50":"#f7fbff",
-        "100":"#dbe9fc",
-        "200":"#000000",
-        "300":"#000000",
-        "400":"#000000",
-        "500":"#000000",
-        "600":"#000000",
-        "700":"#000000",
-        "800":"#000000",
-        "900":"#000000",
-        "950":"#000000"
-    }
+	light: {
+		"theme-50":"#f7fbff",
+		"theme-100":"#f0f6ff",
+		"theme-200":"#e0eeff",
+		"theme-300":"#d1e5ff",
+		"theme-400":"#c2dcff",
+		"theme-500":"#b3d4ff",
+		"theme-600":"#6babff",
+		"theme-700":"#2483ff",
+		"theme-800":"#005fdb",
+		"theme-900":"#004094",
+		"theme-950":"#003170"
+	},
+	dark: {
+		"theme-50":"#f7fbff",
+		"theme-100":"#dbe9fc",
+		"theme-200":"#000000",
+		"theme-300":"#000000",
+		"theme-400":"#000000",
+		"theme-500":"#000000",
+		"theme-600":"#000000",
+		"theme-700":"#000000",
+		"theme-800":"#000000",
+		"theme-900":"#000000",
+		"theme-950":"#000000"
+	}
 }
 
 /*
@@ -56,6 +56,8 @@ const themes = {
 
 */
 
+
+
 // Update #pageThemeHeading text
 function updatePageThemeHeading(){
 	let headingToUpdate = document.getElementById("pageThemeHeading");
@@ -80,32 +82,36 @@ function updatePageThemeButtonText(){
 
 // Add onclick to #pageThemeButton
 function togglePageTheme(){
-    if (getStoredPageTheme() == "dark"){
-        pageTheme = "light";
-    } else {
-        pageTheme = "dark";
-    }
-    setPageThemeToStorage();
-
-    applySavedTheme();
+	if (getStoredPageTheme() == "dark"){
+		pageTheme = "light";
+	} else {
+		pageTheme = "dark";
+	}
+	setPageThemeToStorage();
+	
+	applySavedTheme();
 }
+
 
 // Apply theme from localstorage
 function applySavedTheme(){
-    updatePageThemeButtonText();
-    updatePageThemeHeading();
-    updateCssVariables();
+	updatePageThemeButtonText();
+	updatePageThemeHeading();
+	updateCssVariables();
 }
+
 
 // Update CSS variables based on current theme 
 function updateCssVariables(){
-    let themeName = getStoredPageTheme();
-    // for every CSS variable in themes["light"]
-    for (const variable in themes[themeName]){
-        document.documentElement.style.setProperty(`--${variable}`, themes[themeName][variable]);
-        console.log("Updated CSS variable --" + variable);
-    }
+	let themeName = getStoredPageTheme();
+	// for every CSS variable in themes["light"]
+	for (const variable in themes[themeName]){
+		document.documentElement.style.setProperty(`--${variable}`, themes[themeName][variable]);
+		console.log("Updated CSS variable --" + variable);
+
+	}
 }
+
 
 let pageThemeToggleButton = document.getElementById("pageThemeButton");
 pageThemeToggleButton.addEventListener("click", togglePageTheme);
